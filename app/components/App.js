ede475cdf3730c7af3cce71c989e011481d2766b
Rupehra Chouhan <rupehra1@gmail.com>
2017-06-27 12:38:36 -0600

    ADD: Components

--- app/components/App.js
+++ app/components/App.js
@@ -3,6 +3,9 @@ var ReactDOM = require('react-dom');
 
 //At first I thought that you cannot export and element...
 //turns out you can..
-const appElement = <p>This is an element. You can export them and they are immutable!</p>;
+const appElement = <div>
+  <h1>Elements</h1>
+  <p>This is an element. You can export them and they are immutable!</p>
+  </div>;
 
 module.exports = appElement;
--- app/index.js
+++ app/index.js
@@ -8,11 +8,11 @@ ReactDOM.render(appElement, document.getElementById('element'));
 function tick() {
   const elem = (
     <div>
-      <br />
       <p>The only way to update the UI is to create a new element.
         Put the element inside a function to create a component and
-        then call the function otherwise it won't show!!</p>
-      <h2>It is {new Date().toLocaleTimeString()}. </h2>
+        then call the function otherwise it won't show!! Below is an
+      example of updating UI.</p>
+      <h3>It is {new Date().toLocaleTimeString()}. </h3>
     </div>
   );
   ReactDOM.render(
@@ -20,11 +20,42 @@ function tick() {
     document.getElementById('tick')
   );
 }
+setInterval(tick, 1000);
+
 function WelcomeComponent(props) {
   return (
     <div>
-      <h1>Welcome, {pros.name}</h1>);
+      <h1>Elements and Components</h1>
+      <p>In addition to representing DOM tags, elements can also represent
+      user-defined component. In the heading below, a component with prop
+      name="Rupehra" was assigned to an element.</p>
+      <h3>Welcome, {props.name}</h3>
     </div>
   );
 }
-setInterval(tick, 1000);
+const element = <WelcomeComponent name="Rupehra" />;
+ReactDOM.render(
+  element,
+  document.getElementById('elementRepresentingComponent')
+);
+
+function SayHi(props) {
+  return (<p> Hi {props.name}!</p>);
+}
+
+function CallSayHi() {
+  return (
+    <div>
+      <h1>Composing Components</h1>
+      <p>Components can refer to other components in the output. Below
+      I am calling a component multiple times with different props.</p>
+      <SayHi name="Qamar" />
+      <SayHi name="Chaittali" />
+      <SayHi name="Alison" />
+    </div>
+  );
+}
+ReactDOM.render(
+  <CallSayHi />,
+  document.getElementById('composingComponent')
+);
