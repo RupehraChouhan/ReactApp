2fd3d519fdc571ae1e3c2ed6305a9aeda767b88a
Rupehra Chouhan <rupehra1@gmail.com>
2017-06-28 23:27:02 -0600

    MOD: Added a boostrap panel

--- app/components/App.js
+++ app/components/App.js
@@ -1,11 +1,17 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
+// import {Panel} from 'react-bootstrap';
 
 //At first I thought that you cannot export and element...
 //turns out you can..
-const appElement = <div>
-  <h1>Elements</h1>
-  <p>This is an element. You can export them and they are immutable!</p>
-  </div>;
+
+
+const appElement = (
+  <div className="panel panel-default">
+    <div className="panel-heading">Elements</div>
+    <div className="panel-body">This is an element. You can export them and they are
+       immutable!</div>
+  </div>
+);
 
 module.exports = appElement;
